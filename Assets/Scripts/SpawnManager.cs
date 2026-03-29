f727338fec35ddb31ce10a9b39730439d9c70af2
greenmasala <sinchai2549@gmail.com>
2026-03-30 00:57:21 +0700

    Assignment 3

--- Assets/Scripts/SpawnManager.cs
+++ Assets/Scripts/SpawnManager.cs
@@ -1,71 +1,93 @@
 using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 public class SpawnManager : MonoBehaviour
 {
-    public Transform[] spawnPoints;
-    public GameObject enemyPrefab;
-
-    //private Coroutine ByeRoutine;
+    public Transform[] SpawnPoints;
+    public GameObject[] PowerUps;
+    public GameObject Enemy;
+    public Wave[] Waves;
+    private int currentWave = 0;
+    private Transform[] waveSpawnPoints;
+    private Coroutine spawnCoroutine;
 
     void Start()
     {
-        //InvokeRepeating(nameof(RandomSpawn), 0, 2f);
-        //StartCoroutine(Hello());
-        //ByeRoutine = StartCoroutine(Bye());   
-        StartCoroutine(SpawnRoutine());
+        StartCoroutine(WaveManager());
     }
 
     private void Update()
     {
-        //if (Time.time > 3)
-        //{
-        //    //StopAllCoroutines();
-        //    StopCoroutine(ByeRoutine); //these are pretty self explanatory
-        //}
+        
     }
 
-    void RandomSpawn()
+    IEnumerator WaveManager()
     {
-        var index = Random.Range(0, spawnPoints.Length);
-        var spawnPoint = spawnPoints[index];
-        Instantiate(enemyPrefab, spawnPoint.position, Quaternion.identity);
-    }
-
-    IEnumerator SpawnRoutine()
-    {
-        while (true)
+        while (currentWave < Waves.Length)
         {
-            RandomSpawn();
-            yield return new WaitForSeconds(3);
+            Debug.Log("Current wave: " + currentWave);
+            SelectSpawnPoints(currentWave);
+
+            for (int i = 0; i < Waves[currentWave].NumberOfPowerUp; i++)
+            {
+                RandomPowerUp();
+            }
+
+            spawnCoroutine = StartCoroutine(SpawnRoutine(currentWave));
+            yield return spawnCoroutine;
+            currentWave++;
+        }
+        Debug.Log("All waves completed!");
+    }
+
+    void SelectSpawnPoints(int waveIndex)
+    {
+        int count = Waves[waveIndex].NumberOfRandomSpawnPoint;
+        waveSpawnPoints = new Transform[count];
+        List<Transform> availableSpawnPoints = new List<Transform>(SpawnPoints);
+
+        for (int i = 0; i < count; i++)
+        {
+            int randomIndex = Random.Range(0, availableSpawnPoints.Count);
+            Debug.Log(randomIndex);
+            waveSpawnPoints[i] = availableSpawnPoints[randomIndex];
+            availableSpawnPoints.RemoveAt(randomIndex);
         }
     }
 
-    //IEnumerator Hello(float delay)
-    //{
-    //    yield return new WaitForSeconds(delay);
+    void RandomSpawn(int waveIndex)
+    {
+        if (waveSpawnPoints == null || waveSpawnPoints.Length == 0)
+        {
+            return;
+        }
 
-    //    Debug.Log("Hello" + Time.frameCount); //1st frame
-    //    yield return null; //waits one frame
-    //    Debug.Log("Hello" + Time.frameCount); //2nd frame
-    //}
+        var spawnPoint = waveSpawnPoints[Random.Range(0, waveSpawnPoints.Length)];
+        Instantiate(Enemy, spawnPoint.position, Quaternion.identity);
+    }
 
-    //IEnumerator Bye()
-    //{
-    //    while (true)
-    //    {
-    //        Debug.Log("Bye" + Time.frameCount + "" + Time.time);
-    //        yield return new WaitForSeconds(1);
+    void RandomPowerUp()
+    {
+        if (PowerUps.Length == 0 || SpawnPoints.Length == 0)
+        {
+            return;
+        }
 
-    //        yield return Hello(4); //this is how you call coroutines in coroutines, though this will wait for the coroutine to finish first
-    //        //StartCoroutine(Hello()) this one wont wait for Hello() to finish first
-    //        yield return new WaitForSeconds(1);
-    //        //yield return null; //loops but skips one frame, frame count increments infinitely
+        var spawnPoint = SpawnPoints[Random.Range(0, SpawnPoints.Length)];
+        var powerUpPrefab = PowerUps[Random.Range(0, PowerUps.Length)];
+        Instantiate(powerUpPrefab, spawnPoint.position, Quaternion.identity);
+    }
 
-    //        if (Time.time > 5)
-    //        {
-    //            yield break; //stop coroutine within itself //normal break; is for getting out of loops
-    //        }
-    //    }
-    //}
+    IEnumerator SpawnRoutine(int waveIndex)
+    {
+        yield return new WaitForSeconds(Waves[waveIndex].DelayStart);
+
+        for (int i = 0; i < Waves[waveIndex].TotalSpawnEnemies; i++)
+        {
+            RandomSpawn(waveIndex);
+            yield return new WaitForSeconds(Waves[waveIndex].SpawnInterval);
+        }
+    }
 }
---
+++ Assets/Scripts/Wave.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+
+[System.Serializable]
+public class Wave
+{
+    public int TotalSpawnEnemies;
+    public int NumberOfRandomSpawnPoint;
+    public float DelayStart;
+    public float SpawnInterval;
+    public int NumberOfPowerUp;
+}
